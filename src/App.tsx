1e49296a67454d7e57fb79b886a3ec555169f2e8
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-26 21:57:14 +0200

    clean project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import { UserWarning } from './UserWarning';
+import { TodoApp } from './components/TodoApp';
+import { TodoAppContext } from './TodoStore';
+
+const USER_ID = 11722;
+
+export const App: React.FC = () => {
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  return (
+    <TodoAppContext>
+      <TodoApp />
+    </TodoAppContext>
+  );
+};
